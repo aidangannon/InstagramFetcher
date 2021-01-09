009d08351975b446e9797ee1c061cef3f1d0c03f
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-01-09 17:58:22 +0000

    implemented error meta deserialization

--- src/DataAccess/Dtos/Serializers/ErrorMetaDtoSerializer.php
+++ src/DataAccess/Dtos/Serializers/ErrorMetaDtoSerializer.php
@@ -4,15 +4,33 @@
 namespace InstaFetcher\DataAccess\Dtos\Serializers;
 
 
-use BadMethodCallException;
 use InstaFetcher\DataAccess\Dtos\ErrorMetaDataDto;
+use InstaFetcher\DataAccess\Dtos\Serializers\Exception\ErrorDtoDeserializationError;
 use InstaFetcher\Interfaces\DataAccess\DtoSerializer\IErrorMetaDtoSerializer;
+use TypeError;
 
 class ErrorMetaDtoSerializer implements IErrorMetaDtoSerializer
 {
 
     public function deserialize(array $errorMeta): ErrorMetaDataDto
     {
-        throw new BadMethodCallException("not implemented");
+
+        try {
+            if (
+                (isset($errorMeta[ErrorMetaDataDto::TYPE_FIELD])) &&
+                (isset($errorMeta[ErrorMetaDataDto::CODE_FIELD])) &&
+                (isset($errorMeta[ErrorMetaDataDto::SUB_CODE_FIELD]))
+            ) {
+                $type = $errorMeta[ErrorMetaDataDto::TYPE_FIELD];
+                $code = $errorMeta[ErrorMetaDataDto::CODE_FIELD];
+                $subCode = $errorMeta[ErrorMetaDataDto::SUB_CODE_FIELD];
+                return new ErrorMetaDataDto($type, $code, $subCode);
+            } else {
+                throw new ErrorDtoDeserializationError();
+            }
+        }
+        catch(TypeError $e){
+            throw new ErrorDtoDeserializationError();
+        }
     }
 }
